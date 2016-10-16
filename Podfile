50fe5d42bd43422b460a6eb3320741fd85ef87ec
Zhanou Liang <gusliazh@student.gu.se>
2016-10-16 17:56:50 +0200

    pods files

---
+++ Podfile
@@ -0,0 +1,21 @@
+# Uncomment this line to define a global platform for your project
+ platform :ios, '10.0'
+
+target 'Prototype1' do
+  # Comment this line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+	pod 'CocoaMQTT'
+end
+  # Pods for Prototype1
+
+  target 'Prototype1Tests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'Prototype1UITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+
---
+++ Prototype1/ChatMessageViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ChatMessageViewController.swift
+//  Prototype1
+//
+//  Created by LiangZhanou on 2016-10-16.
+//  Copyright © 2016 LiangZhanou. All rights reserved.
+//
+
+import UIKit
+
+class ChatMessageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
